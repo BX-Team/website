61b453018d7560448dfa15ffdb8e06630b9a8baa
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-02-13 00:43:57 +0300

    refactor: switch to monorepo

---
+++ apps/website/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'BX Team',
+  url: 'https://bxteam.org',
+  description:
+    'BX Team is a small development team that mainly specializes in software for the Minecraft and other games.',
+  links: {
+    discord: 'https://discord.gg/p7cxhw7E2M',
+    github: 'https://github.com/BX-Team',
+    projects: {
+      divinemc: 'https://github.com/DivineMC/DivineMC',
+    },
+  },
+} as const;
