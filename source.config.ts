4ad4bf2dd50bd5285544c4a83528a629bad557e8
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-02-17 00:26:51 +0300

    feat: migrate configuration to TypeScript, add new plugins and update package dependencies

--- source.config.ts
+++ source.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import { remarkMermaid } from '@theguild/remark-mermaid';
+import { remarkAdmonition } from 'fumadocs-core/mdx-plugins';
+import { defineConfig, defineDocs, frontmatterSchema } from 'fumadocs-mdx/config';
+import { z } from 'zod';
 
 export const docs = defineDocs({
   dir: 'content/docs',
+  docs: {
+    schema: frontmatterSchema.extend({
+      authors: z.array(z.string()).optional(),
+    }),
+  },
 });
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    remarkPlugins: [remarkMermaid, remarkAdmonition],
   },
 });
