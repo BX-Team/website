7ab3e62b6ca88f5fdff0eed0ab56e736db3c98bb
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-02-17 15:52:40 +0300

    feat: update documentation structure; finish DivineMC docs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,26 @@
 import type { ReactNode } from 'react';
 
+import Image from 'next/image';
+
 import { baseOptions } from '@/app/layout.config';
 import { DocsLayout } from '@/components/notebook';
 import { source } from '@/lib/source';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions} disableThemeSwitch={true}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions}
+      disableThemeSwitch={true}
+      nav={{
+        title: (
+          <div className='flex items-center gap-2'>
+            <Image src='/logo.png' alt='BX Team Logo' width={22} height={22} />
+            <span>BX Team Documentation</span>
+          </div>
+        ),
+      }}
+    >
       {children}
     </DocsLayout>
   );
